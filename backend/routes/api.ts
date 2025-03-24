5c6e99679d982c04905d0e3d83b0deefacf52a45
Ichiroki <fahrezirizqiawan12649@gmail.com>
2025-03-24 11:19:51 +0700

    Update: change pdf layout

--- backend/routes/api.ts
+++ backend/routes/api.ts
@@ -1,33 +1,38 @@
-import express, { Request, Response } from 'express';
-import PDFDocument from 'pdfkit';
+import express, { Request, Response } from 'express'
+import PDFDocument from 'pdfkit-table'
 
 const Api = express()
 
 Api.post('/generate-pdf', (req: Request, res: Response) => {
     const user = req.body
 
-    let randNumb = Array.from({ length: 6 }, () => Math.floor(Math.random() * 10)).join('')
-
     res.setHeader('Content-Disposition', `attachment; filename=undangan.pdf`)
     res.setHeader('Content-Type', 'application/pdf')
 
-    const doc = new PDFDocument()
+    const doc = new PDFDocument({
+        lang: 'id_ID',
+        size: 'A5',
+        layout: 'landscape'
+    })
     doc.pipe(res)
+
+    const table = {
+        rows: [
+            [`ID`, `:`, `${user.id}`],
+            [`Nama`, `:`, `${user.nama}`],
+            [`Kota`, `:`, `${user.kota}`],
+            [`Handphone`, `:`, `${user.handphone}`],
+            [`Tamu`, `:`, `${user.tamu}`],
+            [`Hubungan`, `:`, `${user.hubungan}`],
+        ]
+    }
 
     doc
         .fontSize(14)
         .text("Pengundang yang beridentitas dibawah ini:", { align: "center" })
         .moveDown()
 
-    doc
-        .fontSize(12)
-        .text(`ID              : ${user.id}`)
-        .text(`Nama            : ${user.nama}`)
-        .text(`Kota Asal       : ${user.kota}`)
-        .text(`Handphone       : ${user.handphone}`)
-        .text(`Jumlah Tamu     : ${user.tamu}`)
-        .text(`Hubungan dengan Mempelai : ${user.hubungan}`)
-        .moveDown(2)
+    doc.table(table)
 
     doc
         .fontSize(14)
